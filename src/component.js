4b7acada13c2de94f59e48e6932afc000638acca
Dániel Kántor <git@daniel-kantor.com>
2019-08-19 18:43:30 +0200

    refactor: create function wrapComponentWithTitle

--- src/component.js
+++ src/component.js
@@ -2,13 +2,16 @@ const YAML = require("yaml");
 const _ = YAML.stringify;
 const $ = YAML.parse;
 
+const wrapComponentWithTitle = component => title =>
+  _({
+    rows: [{ "h1 text": title }, $(component)]
+  });
+
 const createComponent = (chartType, chartTitle = null) => data =>
   chartTitle === null
     ? _({
         [chartType]: data
       })
-    : _({
-        rows: [{ "h1 text": chartTitle }, $(createComponent(chartType)(data))]
-      });
+    : wrapComponentWithTitle(createComponent(chartType)(data))(chartTitle);
 
 module.exports = createComponent;
