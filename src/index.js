96ae919893b3353cad60a13a4f5fc14564ddd2f9
Dániel Kántor <git@daniel-kantor.com>
2019-08-25 14:47:09 +0200

    feat: make chart type parametric

--- src/index.js
+++ src/index.js
@@ -5,50 +5,54 @@ const fs = require("fs");
 const { spawn } = require("child_process");
 const getStdin = require("get-stdin");
 
-const createTemporaryFile = dashboard => {
-  const tempFile = tmp.fileSync({ postfix: ".yml" });
-  fs.writeSync(tempFile.fd, dashboard);
-  return tempFile.name;
-};
+const createJustChartCommand = chartType => {
+  const createTemporaryFile = dashboard => {
+    const tempFile = tmp.fileSync({ postfix: ".yml" });
+    fs.writeSync(tempFile.fd, dashboard);
+    return tempFile.name;
+  };
 
-const showDashboard = dashboard => {
-  const child = spawn("just-dashboard", [createTemporaryFile(dashboard)]);
-};
+  const showDashboard = dashboard => {
+    const child = spawn("just-dashboard", [createTemporaryFile(dashboard)]);
+  };
 
-const parseInput = inputData =>
-  inputData.split("\n").map(line => line.split("\t"));
+  const parseInput = inputData =>
+    inputData.split("\n").map(line => line.split("\t"));
 
-const compileDashboard = inputData => {
-  const component = createComponent("bar chart")({
-    rows: parseInput(inputData)
-  });
-  return createDashboard("")([component]);
-};
+  const compileDashboard = inputData => {
+    const component = createComponent(chartType)({
+      rows: parseInput(inputData)
+    });
+    return createDashboard("")([component]);
+  };
 
-class JustChartCommand extends Command {
-  async run() {
-    const inputData = await getStdin();
-    const { flags } = this.parse(JustChartCommand);
-    const dashboard = compileDashboard(inputData);
+  class JustChartCommand extends Command {
+    async run() {
+      const inputData = await getStdin();
+      const { flags } = this.parse(JustChartCommand);
+      const dashboard = compileDashboard(inputData);
 
-    if (flags.show) {
-      showDashboard(dashboard);
-    } else {
-      process.stdout.write(dashboard);
+      if (flags.show) {
+        showDashboard(dashboard);
+      } else {
+        process.stdout.write(dashboard);
+      }
     }
   }
-}
 
-JustChartCommand.description = `Create charts`;
+  JustChartCommand.description = `Create ${chartType}s`;
 
-JustChartCommand.flags = {
-  version: flags.version({ char: "v" }),
-  help: flags.help({ char: "h" }),
-  show: flags.boolean({
-    char: "s",
-    default: false,
-    description: "Display chart graphically"
-  })
+  JustChartCommand.flags = {
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    show: flags.boolean({
+      char: "s",
+      default: false,
+      description: "Display chart graphically"
+    })
+  };
+
+  return JustChartCommand;
 };
 
-module.exports = JustChartCommand;
+module.exports = createJustChartCommand;
